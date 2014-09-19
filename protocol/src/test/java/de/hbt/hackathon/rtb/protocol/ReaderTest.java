75869c504b361c68b7d0d59100aab454ac8c4d2c
Klaus Richarz <kr@Infrastrucktur-Gruppes-MacBook-Pro.local>
2014-09-19 11:53:10 +0200

    reader test

--- protocol/src/test/java/de/hbt/hackathon/rtb/protocol/ReaderTest.java
+++ protocol/src/test/java/de/hbt/hackathon/rtb/protocol/ReaderTest.java
@@ -1,22 +1,22 @@
 package de.hbt.hackathon.rtb.protocol;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.hbt.hackathon.rtb.protocol.message.AngleType;
 import de.hbt.hackathon.rtb.protocol.message.input.InputMessage;
+import de.hbt.hackathon.rtb.protocol.message.input.RotationReachedMessage;
 import de.hbt.hackathon.rtb.protocol.message.input.YourNameMessage;
 
 public class ReaderTest {
 	
 	private static InputStream realSystemIn = null;
-	private Reader reader;
 	
 	@BeforeClass
 	public static void beforeClass() {
@@ -28,24 +28,32 @@ public class ReaderTest {
 		System.setIn(realSystemIn);
 	}
 	
-	@Before
-	public void init() {
-		reader = new Reader();
-	}
-
 	@Test
-	public void testRead01() throws UnsupportedEncodingException {
+	public void testRead01() throws IOException {
 		ByteArrayInputStream bin = new ByteArrayInputStream(("YourName brain-bugs" + System.lineSeparator()).getBytes("ASCII"));
-		System.out.println(bin.available());
 		System.setIn(bin);
+		Reader reader = new Reader();
 		
-		System.out.println(System.in.getClass());
-		
-		//Assert.assertEquals(true, reader.ready());
+		Assert.assertEquals(true, reader.ready());
 		InputMessage message = reader.read();
 		Assert.assertEquals(YourNameMessage.class, message.getClass());
 		YourNameMessage ynm = (YourNameMessage) message;
 		Assert.assertEquals("brain-bugs", ynm.getName());
+	}
+	
+	@Test
+	public void testRead02() throws IOException {
+		ByteArrayInputStream bin = new ByteArrayInputStream(("RotationReached 3" + System.lineSeparator()).getBytes("ASCII"));
+		System.setIn(bin);
+		Reader reader = new Reader();
+		
+		Assert.assertEquals(true, reader.ready());
+		InputMessage message = reader.read();
+		Assert.assertEquals(RotationReachedMessage.class, message.getClass());
+		RotationReachedMessage rrm = (RotationReachedMessage) message;
+		Assert.assertEquals(2, rrm.getReachedAngles().size());
+		Assert.assertEquals(true, rrm.getReachedAngles().contains(AngleType.ROBOT));
+		Assert.assertEquals(true, rrm.getReachedAngles().contains(AngleType.CANNON));
 	}
 
 }

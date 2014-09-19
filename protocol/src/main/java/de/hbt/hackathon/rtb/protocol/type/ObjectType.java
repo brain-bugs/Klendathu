7219d8ac6fb83d5368a32ef9bcde1f356ea77dd7
Klaus Richarz <kr@Infrastrucktur-Gruppes-MacBook-Pro.local>
2014-09-19 10:09:15 +0200

    object type correction

--- protocol/src/main/java/de/hbt/hackathon/rtb/protocol/type/ObjectType.java
+++ protocol/src/main/java/de/hbt/hackathon/rtb/protocol/type/ObjectType.java
@@ -2,14 +2,13 @@ package de.hbt.hackathon.rtb.protocol.type;
 
 public enum ObjectType {
 
-	NOOBJECT(1), ROBOT(0), SHOT(1), WALL(2), COOKIE(3), MINE(4), LAST_OBJECT_TYPE(5);
+	NOOBJECT(-1), ROBOT(0), SHOT(1), WALL(2), COOKIE(3), MINE(4), LAST_OBJECT_TYPE(5);
 
 	private final int code;
 
 	private ObjectType(int code) {
 		this.code = code;
 	}
-
 	public int getCode() {
 		return code;
 	}

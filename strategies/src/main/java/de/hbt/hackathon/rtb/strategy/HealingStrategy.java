ee1cb621e75dc55e377b8f41820e45f56137a06c
ilja82 <iljaerbis@gmx.de>
2014-09-20 00:17:19 +0200

    Targeter improved

--- strategies/src/main/java/de/hbt/hackathon/rtb/strategy/HealingStrategy.java
+++ strategies/src/main/java/de/hbt/hackathon/rtb/strategy/HealingStrategy.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.hbt.hackathon.rtb.base.geo.GeometricSet;
 import de.hbt.hackathon.rtb.base.message.AngleType;
 import de.hbt.hackathon.rtb.base.message.output.AccelerateMessage;
 import de.hbt.hackathon.rtb.base.message.output.BrakeMessage;
@@ -15,7 +16,9 @@ import de.hbt.hackathon.rtb.base.message.output.OutputMessage;
 import de.hbt.hackathon.rtb.base.message.output.RotateAmountMessage;
 import de.hbt.hackathon.rtb.base.strategy.AbstractStrategy;
 import de.hbt.hackathon.rtb.world.Cookie;
+import de.hbt.hackathon.rtb.world.Mine;
 import de.hbt.hackathon.rtb.world.MyRobot;
+import de.hbt.hackathon.rtb.world.Robot;
 import de.hbt.hackathon.rtb.world.World;
 
 public class HealingStrategy extends AbstractStrategy {
@@ -34,36 +37,30 @@ public class HealingStrategy extends AbstractStrategy {
 	public List<OutputMessage> process() {
 		List<OutputMessage> messages = new ArrayList<OutputMessage>();
 
+		MyRobot myRobot = world.getMyRobot();
 		Set<Cookie> cookies = world.getCookies();
+		GeometricSet<Mine> mines = world.getMines();
+		GeometricSet<Robot> robots = world.getRobots();
 		if (!cookies.isEmpty()) {
 			LOGGER.info("There are cookies!");
-			MyRobot myRobot = world.getMyRobot();
 			if (myRobot != null) {
 				Cookie cookie = cookies.iterator().next();
 				Targeter targeter = new Targeter();
-				targeter.setOwnLocation(myRobot.getCurrentPosition(), AngleUtils.normalizeAngle(myRobot.getRotationAngle()));
-				double rotationAngle = AngleUtils.normalizeAngle(targeter.calculateAngleTowards(cookie.getCurrentPosition()));
-				double distance = cookie.getCurrentPosition().distance(myRobot.getCurrentPosition());
-				double angleOffset = (Math.toDegrees(rotationAngle) < 180d) ? 1d : -1d;
-
-				LOGGER.info("distance to cookie: " + distance + ", angle: " + Math.toDegrees(rotationAngle));
-				LOGGER.debug("My position: " + myRobot.getCurrentPosition() + ", angle: "
-						+ Math.toRadians(AngleUtils.normalizeAngle(myRobot.getRotationAngle())) + ", speed: " + myRobot.getSpeed());
-				LOGGER.info("Cookie position: " + cookie.getCurrentPosition());
-				RotateAmountMessage rm = new RotateAmountMessage(EnumSet.of(AngleType.ROBOT), 1.0, angleOffset);
-				LOGGER.info("Rotate by " + angleOffset + " radians");
-				messages.add(rm);
-				if ((myRobot.getSpeed() > 0.5) && (Math.toDegrees(rotationAngle) > 20) && (Math.toDegrees(rotationAngle) < 340)) {
-					BrakeMessage bm = new BrakeMessage(1.0);
-					LOGGER.info("Full Break");
-					messages.add(bm);
-				} else {
-					AccelerateMessage am = new AccelerateMessage(1.0);
-					LOGGER.info("Accelerate by 1.0");
-					messages.add(am);
-					BrakeMessage bm = new BrakeMessage(0.0);
-					messages.add(bm);
-				}
+				targeter.driveTo(myRobot, cookie.getCurrentPosition());
+			}
+		} else if (!mines.isEmpty()) {
+			LOGGER.info("There are mines!");
+			if (myRobot != null) {
+				Mine mine = mines.iterator().next();
+				Targeter targeter = new Targeter();
+				targeter.aimCannonToAndShoot(myRobot, mine.getCurrentPosition());
+			}
+		} else if (!robots.isEmpty()) {
+			LOGGER.info("There are robots!");
+			if (myRobot != null) {
+				Robot robot = robots.iterator().next();
+				Targeter targeter = new Targeter();
+				targeter.aimCannonToAndShoot(myRobot, robot.getCurrentPosition());
 			}
 		} else {
 			RotateAmountMessage rm = new RotateAmountMessage(EnumSet.of(AngleType.ROBOT), 1.0, 1.0);
--- strategies/src/main/java/de/hbt/hackathon/rtb/strategy/Targeter.java
+++ strategies/src/main/java/de/hbt/hackathon/rtb/strategy/Targeter.java
@@ -1,8 +1,23 @@
 package de.hbt.hackathon.rtb.strategy;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.hbt.hackathon.rtb.base.message.AngleType;
+import de.hbt.hackathon.rtb.base.message.output.AccelerateMessage;
+import de.hbt.hackathon.rtb.base.message.output.BrakeMessage;
+import de.hbt.hackathon.rtb.base.message.output.OutputMessage;
+import de.hbt.hackathon.rtb.base.message.output.RotateAmountMessage;
+import de.hbt.hackathon.rtb.base.message.output.ShootMessage;
 import de.hbt.hackathon.rtb.base.type.Coordinate;
+import de.hbt.hackathon.rtb.world.MyRobot;
 
 public class Targeter {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Targeter.class);
 
 	private Coordinate ownPosition;
 	private double ownAngle;
@@ -15,6 +30,57 @@ public class Targeter {
 	public double calculateAngleTowards(Coordinate target) {
 		double targetAngle = AngleUtils.getAngle(ownPosition.getX(), ownPosition.getY(), target.getX(), target.getY());
 		return targetAngle - ownAngle;
+	}
+	
+	public List<OutputMessage> driveTo(MyRobot myRobot, Coordinate targetCoordinate) {
+		List<OutputMessage> messages = new ArrayList<OutputMessage>();
+		this.setOwnLocation(myRobot.getCurrentPosition(), AngleUtils.normalizeAngle(myRobot.getRotationAngle()));
+		double rotationAngle = AngleUtils.normalizeAngle(this.calculateAngleTowards(targetCoordinate));
+		double distance = targetCoordinate.distance(myRobot.getCurrentPosition());
+		double angleOffset = (Math.toDegrees(rotationAngle) < 180d) ? 1d : -1d;
+
+		LOGGER.info("distance to target: " + distance + ", angle: " + Math.toDegrees(rotationAngle));
+		LOGGER.debug("My position: " + myRobot.getCurrentPosition() + ", angle: "
+				+ Math.toRadians(AngleUtils.normalizeAngle(myRobot.getRotationAngle())) + ", speed: " + myRobot.getSpeed());
+		LOGGER.info("Target position: " + targetCoordinate);
+		RotateAmountMessage rm = new RotateAmountMessage(EnumSet.of(AngleType.ROBOT), 1.0, angleOffset);
+		LOGGER.info("Rotate by " + angleOffset + " radians");
+		messages.add(rm);
+		if ((myRobot.getSpeed() > 0.5) && (Math.toDegrees(rotationAngle) > 20) && (Math.toDegrees(rotationAngle) < 340)) {
+			BrakeMessage bm = new BrakeMessage(1.0);
+			LOGGER.info("Full Break");
+			messages.add(bm);
+		} else {
+			AccelerateMessage am = new AccelerateMessage(1.0);
+			LOGGER.info("Accelerate by 1.0");
+			messages.add(am);
+			BrakeMessage bm = new BrakeMessage(0.0);
+			messages.add(bm);
+		}
+		
+		return messages;
+	}
+	
+	public List<OutputMessage> aimCannonToAndShoot(MyRobot myRobot, Coordinate targetCoordinate) {
+		List<OutputMessage> messages = new ArrayList<OutputMessage>();
+		this.setOwnLocation(myRobot.getCurrentPosition(), AngleUtils.normalizeAngle(myRobot.getCannonAngle()));
+		double rotationAngle = AngleUtils.normalizeAngle(this.calculateAngleTowards(targetCoordinate));
+		double angleOffset = (Math.toDegrees(rotationAngle) < 180d) ? 1d : -1d;
+
+		LOGGER.info("angle to target: " + Math.toDegrees(rotationAngle));
+		LOGGER.debug("My position: " + myRobot.getCurrentPosition() + ", cannon angle: "
+				+ Math.toRadians(AngleUtils.normalizeAngle(myRobot.getCannonAngle())));
+		LOGGER.info("Target position: " + targetCoordinate);
+		
+		if (rotationAngle > 175d && rotationAngle < 185d) {
+			messages.add(new ShootMessage(1));
+		}
+			
+		RotateAmountMessage rm = new RotateAmountMessage(EnumSet.of(AngleType.CANNON), 1.0, angleOffset);
+		LOGGER.info("Rotate by " + angleOffset + " radians");
+		messages.add(rm);
+		
+		return messages;
 	}
 
 }

189ff82d90991440b8684b5584fd90629b02c255
sku <sku@CLW175.corp.hbt.de>
2014-09-19 12:13:33 +0200

    added robot world details

--- world/src/main/java/de/hbt/hackathon/rtb/world/MyRobot.java
+++ world/src/main/java/de/hbt/hackathon/rtb/world/MyRobot.java
@@ -4,8 +4,99 @@ import de.hbt.hackathon.rtb.base.Coordinate;
 
 public class MyRobot extends Robot {
 
+	private double acceleration;
+	private double breakFraction;
+	private double rotationAngle;
+	private double radarAngle;
+	private double cannonSweepAngularVelocity;
+	private double cannonSweepRightAngle;
+	private double cannonSweepLeftAngle;
+	private double radarSweepAngularVelocity;
+	private double radarSweepRightAngle;
+	private double radarSweepLeftAngle;
+
 	public MyRobot(Coordinate coordinate) {
 		super(coordinate, true);
+	}
+
+	public double getAcceleration() {
+		return acceleration;
+	}
+
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+
+	public double getBreakFraction() {
+		return breakFraction;
+	}
+
+	public void setBreakFraction(double breakFraction) {
+		this.breakFraction = breakFraction;
+	}
+
+	public double getRotationAngle() {
+		return rotationAngle;
+	}
+
+	public void setRotationAngle(double rotationAngle) {
+		this.rotationAngle = rotationAngle;
+	}
+
+	public double getRadarAngle() {
+		return radarAngle;
+	}
+
+	public void setRadarAngle(double radarAngle) {
+		this.radarAngle = radarAngle;
+	}
+
+	public double getCannonSweepAngularVelocity() {
+		return cannonSweepAngularVelocity;
+	}
+
+	public void setCannonSweepAngularVelocity(double cannonSweepAngularVelocity) {
+		this.cannonSweepAngularVelocity = cannonSweepAngularVelocity;
+	}
+
+	public double getCannonSweepRightAngle() {
+		return cannonSweepRightAngle;
+	}
+
+	public void setCannonSweepRightAngle(double cannonSweepRightAngle) {
+		this.cannonSweepRightAngle = cannonSweepRightAngle;
+	}
+
+	public double getCannonSweepLeftAngle() {
+		return cannonSweepLeftAngle;
+	}
+
+	public void setCannonSweepLeftAngle(double cannonSweepLeftAngle) {
+		this.cannonSweepLeftAngle = cannonSweepLeftAngle;
+	}
+
+	public double getRadarSweepAngularVelocity() {
+		return radarSweepAngularVelocity;
+	}
+
+	public void setRadarSweepAngularVelocity(double radarSweepAngularVelocity) {
+		this.radarSweepAngularVelocity = radarSweepAngularVelocity;
+	}
+
+	public double getRadarSweepRightAngle() {
+		return radarSweepRightAngle;
+	}
+
+	public void setRadarSweepRightAngle(double radarSweepRightAngle) {
+		this.radarSweepRightAngle = radarSweepRightAngle;
+	}
+
+	public double getRadarSweepLeftAngle() {
+		return radarSweepLeftAngle;
+	}
+
+	public void setRadarSweepLeftAngle(double radarSweepLeftAngle) {
+		this.radarSweepLeftAngle = radarSweepLeftAngle;
 	}
 
 }
--- world/src/main/java/de/hbt/hackathon/rtb/world/Robot.java
+++ world/src/main/java/de/hbt/hackathon/rtb/world/Robot.java
@@ -5,23 +5,44 @@ import de.hbt.hackathon.rtb.base.Coordinate;
 public class Robot extends MovingGameObject {
 
 	private final boolean teamMate;
+
 	private double energyLevel;
-	
+
+	private double speed;
+
+	private double cannonAngle;
+
 	public Robot(Coordinate coordinate, boolean teamMate) {
 		super(coordinate);
 		this.teamMate = teamMate;
 	}
-	
+
 	public boolean isTeamMate() {
 		return teamMate;
 	}
-	
+
 	public double getEnergyLevel() {
 		return energyLevel;
 	}
-	
+
 	public void setEnergyLevel(double energyLevel) {
 		this.energyLevel = energyLevel;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public double getCannonAngle() {
+		return cannonAngle;
+	}
+
+	public void setCannonAngle(double cannonAngle) {
+		this.cannonAngle = cannonAngle;
 	}
 
 }
